4d046af4fa0eae23c999d1fc33e3786f3ebdbe91
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-03-30 18:51:12 +0200

    JOIN'S3

--- files_sql2/select_by_alexey.sql
+++ files_sql2/select_by_alexey.sql
@@ -1,5 +1,6 @@
-select orders.id, date, customer_id, product_name, amount
+select product_name
 from ORDERS
-         RIGHT JOIN customers on ORDERS.customer_id = customers.id
+         RIGHT JOIN  customers on ORDERS.customer_id = customers.id
 WHERE customers.id in (select id from customers where lower(name) = lower('alexey'));
+
 
