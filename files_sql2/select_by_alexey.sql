105f521dfdf30cce23eefdf221c2e817b8319aab
AnastasiyaShark <a.sharkovskaya@yandex.ru>
2021-03-31 17:05:23 +0200

    JOIN'S4

--- files_sql2/select_by_alexey.sql
+++ files_sql2/select_by_alexey.sql
@@ -1,6 +1,6 @@
 select product_name
 from ORDERS
-         RIGHT JOIN  customers on ORDERS.customer_id = customers.id
+         INNER JOIN  customers on ORDERS.customer_id = customers.id
 WHERE customers.id in (select id from customers where lower(name) = lower('alexey'));
 
 
